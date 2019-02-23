d9afb0922c7b0abfd00db64379a3f662b72cc3de
Husain Shaikh <shaikhhusain895@gmail.com>
2019-02-23 19:16:54 +0530

    Update app.js
    Added Abarna's code with changes

--- app.js
+++ app.js
@@ -1,105 +1,115 @@
-
-
-function getFormattedNumber(num)
-{
+function getHistory(){
+	return document.getElementById("history").innerText;
+}
+function printHistory(num){
+	document.getElementById("history").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("output").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output").innerText=num;
+	}
+	else{
+		document.getElementById("output").innerText=getFormattedNumber(num);
+	}	
+}
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
 	var n = Number(num);
 	var value = n.toLocaleString("en");
 	return value;
 }
-
-function getReverseFormattedNumber(num)
-{
-	return parseInt(num.replace(/,/g, ""));
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
 }
-
-function getHistory()
-{
-	return document.getElementById("history").innerText;
-}
-
-function printHistory(num)
-{
-	document.getElementById("history").innerText = (num);
-}
-
-
-function getOutput()
-{
-	return document.getElementById("output").innerText;
-}
-
-function printOutput(num)
-{
-	document.getElementById("output").innerText = getFormattedNumber(num);
-}
-
-var operator1 = document.getElementsByClassName("operator");
-for(var i=0; i<operator1.length; i++)
-{
-	operator1[i].addEventListener('click', function(){
-		if(this.id == "AllClear")
-		{
-			printOutput('');
-			printHistory('');
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="AllClear"){
+			printHistory("");
+			printOutput("");
 		}
-		else if(this.id == "CE")
-		{
-			var output = getReverseFormattedNumber(getOutput()).toString();
-			if(output)
-			{
-				output = output.substr(0, output.length-1);
+		else if(this.id=="CE"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
 				printOutput(output);
 			}
 		}
-
-		/* -----------------broken Code--------------------- */
-		else if (this.id == "plus")
-		{
-			output = getOutput();
-			output = output + " + ";
-			printHistory(output);
-			printOutput(0);
-			output2 = getOutput();
-			if(this.id=="equal")
-			{
-				alert("Got here");
+		else{
+			var output=getOutput();
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
 			}
-
-		}
-		/* ------------------------------------------ */
-	})
-}
-
-var number1 = document.getElementsByClassName("number");
-for(var i=0; i<number1.length; i++)
-{
-	number1[i].addEventListener('click', function(){
-		var output = getReverseFormattedNumber(getOutput());
-		if(output!=NaN)
-		{
-			output = output + this.id;
-			printOutput(output);
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
 		}
 		
-	})
+	});
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
+}
+var microphone = document.getElementById('microphone');
+microphone.onclick=function(){
+	microphone.classList.add("record");
+	var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+	recognition.lang = 'en-US';
+	recognition.start();
+	operations = {"plus":"+",
+				 "minus":"-",
+				 "multiply":"*",
+				 "multiplied":"*",
+				 "divide":"/",
+				 "divided":"/",
+				 "reminder":"%"}
+	
+	recognition.onresult = function(event){
+		var input = event.results[0][0].transcript;
+		for(property in operations){
+			input= input.replace(property, operations[property]);
+		}
+		document.getElementById("output").innerText = input;
+		setTimeout(function(){
+			evaluate(input);
+		},2000);
+		microphone.classList.remove("record");
+	}
+	
+}
+function evaluate(input){
+	try{
+		var result = eval(input);
+		document.getElementById("output").innerText = result;
+	}
+	catch(e){
+		console.log(e);
+		document.getElementById("output").innerText = "";
+	}
+}
